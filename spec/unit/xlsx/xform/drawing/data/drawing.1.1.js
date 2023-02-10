9a1e61a5a0dbb3bd87b6b188639e1aa15ac4abec
Juan Fernando Pastor Ruiz <jfpastor@gmail.com>
2023-02-10 09:58:07 +0100

    'initial'

---
+++ spec/unit/xlsx/xform/drawing/data/drawing.1.1.js
@@ -0,0 +1,43 @@
+module.exports = {
+  anchors: [
+    {
+      anchorType: 'xdr:twoCellAnchor',
+      cellType: 'twoCellAnchor',
+      range: {
+        editAs: 'oneCell',
+        tl: {nativeRow: 0, nativeRowOff: 0, nativeCol: 0, nativeColOff: 0},
+        br: {nativeRow: 7, nativeRowOff: 0, nativeCol: 3, nativeColOff: 0},
+      },
+      picture: {
+        id: '1',
+        name: 'Picture 1',
+        index: 1,
+        rId: 'rId1',
+        hyperlinks: {
+          tooltip: 'exceljs',
+          rId: 'rId3',
+        },
+      },
+    },
+    {
+      anchorType: 'xdr:oneCellAnchor',
+      cellType: 'oneCellAnchor',
+      range: {
+        editAs: 'oneCell',
+        tl: {
+          nativeRow: 2,
+          nativeRowOff: 90000,
+          nativeCol: 5,
+          nativeColOff: 320000,
+        },
+        ext: {width: 100, height: 200},
+      },
+      picture: {
+        id: '2',
+        index: 2,
+        name: 'Picture 2',
+        rId: 'rId2',
+      },
+    },
+  ],
+};
